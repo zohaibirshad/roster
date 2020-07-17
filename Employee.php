62b1f2a162c13d50ba4368e1f8d07f9095a4d735
zohaib irshad <zohaibi619@gmail.com>
2020-07-17 04:05:44 -0700

    Employee model with all relationships

---
+++ Employee.php
@@ -0,0 +1,45 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Employee
+ *
+ * @package App
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $phone
+ * @property string $email
+*/
+class Employee extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['first_name', 'last_name', 'phone', 'email'];
+    public function services()
+    {
+        return $this->belongsToMany('App\Service','employee_service');
+    }
+	
+	public function provides_service($service)
+	{
+		return $this->services()->where('service_id', $service)->exists();
+	}
+	
+	public function working_hours()
+	{
+		return $this->hasMany('App\WorkingHour', 'employee_id');
+	}
+	
+	public function is_working($date) {
+		return $this->working_hours->where('date', '=', $date)->first();
+	}
+	
+	public function service_info($service)
+	{
+		return $this->services()->where('service_id', $service)->first();
+	}
+	
+}
